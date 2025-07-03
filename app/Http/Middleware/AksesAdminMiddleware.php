d5ea4507307c6445048bdd2c03c505f56a4943e7
Henry Jr Zai <hjuniorzai@gmail.com>
2025-07-04 02:23:11 +0700

    fix : memperbaiki middleware untuk admin dapat mengakses  dashboard

--- app/Http/Middleware/AksesAdminMiddleware.php
+++ app/Http/Middleware/AksesAdminMiddleware.php
@@ -15,10 +15,10 @@ class AksesAdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->user() && $request->user()->role === 'admin') {
+        if ($request->user() && $request->user()->hak_akses_id === 1) {
             return $next($request);
         }
-        
+
         return redirect()->back()->with('error', 'Akses ditolak. Anda tidak memiliki izin untuk mengakses halaman ini.');
     }
 }
