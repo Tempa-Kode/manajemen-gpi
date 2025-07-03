4439701e6d9ee1c7046c4c6bae45e38dea79c9b0
Henry Jr Zai <hjuniorzai@gmail.com>
2025-07-04 02:13:05 +0700

    feat : membuat authentikasi login

--- app/Http/Controllers/AutentikasiController.php
+++ app/Http/Controllers/AutentikasiController.php
@@ -3,10 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AutentikasiController extends Controller
 {
     public function login() {
         return view('user.login');
+    }
+
+    public function prosesLogin(Request $request) {
+        $kridensial = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($kridensial)) {
+            $request->session()->regenerate();
+            if (Auth::user()->role == 'admin') {
+                return redirect()->intended('dashboard');
+            } else {
+                return redirect()->intended('home');
+            }
+        }
+
+        return back()->withErrors([
+            'message' => 'Email atau password salah.',
+        ]);
+    }
+
+    public function dashboard() {
+        return view('admin.dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('login', [AutentikasiController::class, 'login'])->name('login');
+Route::post('login', [AutentikasiController::class, 'prosesLogin'])->name('prosesLogin');
+
+Route::get('dashboard', [AutentikasiController::class, 'dashboard'])->middleware(['auth', 'admin'])->name('dashboard');
