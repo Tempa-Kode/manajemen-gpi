c6ebab32f0cae1370e1b22c151fee352671bb783
Henry Jr Zai <hjuniorzai@gmail.com>
2025-07-05 11:23:35 +0700

    fix : memperbaiki bentuk sidebar

--- resources/views/komponent/app.blade.php
+++ resources/views/komponent/app.blade.php
@@ -22,23 +22,9 @@
 
 <body class="g-sidenav-show   bg-gray-100">
     <div class="min-height-300 bg-dark position-absolute w-100"></div>
-    <aside
-        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4" id="sidenav-main">
-        <div class="sidenav-header">
-            <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-                aria-hidden="true" id="iconSidenav"></i>
-            <a class="navbar-brand m-0" href="/dashboard">
-                <img src="../assets/img/logo-gpi-hero.png" width="26px" height="26px" class="navbar-brand-img h-100" alt="logo gpi">
-                <span class="ms-1 font-weight-bold">Manajemen GPI</span>
-            </a>
-        </div>
-        <hr class="horizontal dark mt-0">
-        <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-            <div class="container-fluid py-4">
-                @include('komponent.navigasi-admin')
-            </div>
-        </div>
-    </aside>
+    
+    @include('komponent.navigasi-admin')
+
     <main class="main-content position-relative border-radius-lg ">
         <!-- Navbar -->
         <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
