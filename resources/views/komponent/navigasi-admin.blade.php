a44b0ebb46f4ff7ede8d4b1604d0363a1bc5f803
Henry Jr Zai <hjuniorzai@gmail.com>
2025-07-11 01:29:58 +0700

    fix : memperbaiki route untuk admin warta gereja

--- resources/views/komponent/navigasi-admin.blade.php
+++ resources/views/komponent/navigasi-admin.blade.php
@@ -22,7 +22,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'warta-gereja.index' ? 'active' : '' }}" href="{{ route('admin.warta-gereja.index') }}">
+                <a class="nav-link {{ Route::currentRouteName() == 'warta-gereja.index' ? 'active' : '' }}" href="{{ route('warta-gereja.index') }}">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-app text-dark text-sm opacity-10"></i>
