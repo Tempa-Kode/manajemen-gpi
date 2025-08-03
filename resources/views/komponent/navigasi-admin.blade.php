2d7bc2e29d0d4c999df0a119830790b015d3bf57
Henry Jr Zai <hjuniorzai@gmail.com>
2025-08-03 14:14:54 +0700

    fix : memperbaiki route untuk jadwal ibadah

--- resources/views/komponent/navigasi-admin.blade.php
+++ resources/views/komponent/navigasi-admin.blade.php
@@ -51,8 +51,8 @@
                     <span class="nav-link-text ms-1">Jenis Ibadah</span>
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'jadwal-ibadah.index' ? 'active' : '' }}" href="{{ route('jadwal-ibadah.index') }}">
+            <li class="nav-item {{ Route::currentRouteName() == 'jadwal-ibadah.index' ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('jadwal-ibadah.index') }}">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-world-2 text-dark text-sm opacity-10"></i>
