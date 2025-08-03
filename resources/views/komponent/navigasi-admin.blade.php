24b97f6412074f2aac50d75d10e01f0c29db3e11
Henry Jr Zai <hjuniorzai@gmail.com>
2025-08-03 13:57:34 +0700

    fix : memperbaiki layout navigasi agar sesuai tinggi layar

--- resources/views/komponent/navigasi-admin.blade.php
+++ resources/views/komponent/navigasi-admin.blade.php
@@ -1,5 +1,5 @@
 <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
-    id="sidenav-main">
+    id="sidenav-main" style="height: 100vh;">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
@@ -10,7 +10,7 @@
         </a>
     </div>
     <hr class="horizontal dark mt-0">
-    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main" style="height: 100vh;">
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() == 'dashboard' ? 'active' : '' }}" href="{{ route('dashboard') }}">
